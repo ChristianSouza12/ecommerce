a2c5563e4b8ac8f2eb56c49c07bd911c66e29bb9
ChristianSouza12 <christianlmsouza@outlook.com>
2024-08-30 08:34:02 -0300

    feat: add theme colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,25 @@
+
+
+const Colors = {
+    primary: "rgba(76, 175, 80, 0.7)", // Verde médio com transparência
+    text: {
+        white: "#FFFFFF",  // Branco para texto claro
+        dark: "#2E7D32"    // Verde escuro para texto
+    },
+    background: {
+        dark: "#A5D6A7",   // Verde claro para fundo escuro
+        white: "#E8F5E9"   // Verde muito claro para fundo claro
+    },
+    input: {
+        background: "#C8E6C9",  // Verde claro para fundo de input
+        placeholder: "#66BB6A"  // Verde médio para texto de placeholder
+    },
+    error: "#D32F2F" // Vermelho para mensagens de erro, para destacar bem dos verdes
+};
+
+
+
+
+
+
+export default Colors
