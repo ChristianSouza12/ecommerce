7a325a8611ac8611e628f0df7f63ff5c5e061cb3
ChristianSouza12 <christianlmsouza@outlook.com>
2024-09-04 09:16:39 -0300

    feat: add error message to login page and custom email validation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,10 @@
 import CustomButton from "../components/custom-buttom/custom-buttom.component"
 import CustomInput from "../components/custom-input/custom-input.component"
 import Header from "../components/header/header.component"
+import InputErrorMessage from "../components/input-error-message/input-error-message.component"
 import { LoginContainer, LoginContent, LoginHeadLine, LoginInputContainer, LoginSubtitle } from "./login.styles"
 import {useForm} from "react-hook-form"
-
+import validator from "validator"
 
 import {BsGoogle} from "react-icons/bs"
 import {FiLogIn} from "react-icons/fi"
@@ -41,11 +42,27 @@ const LoginPage = ( ) => {
 
              <LoginInputContainer>
                 <p>E-mail</p>
-                <CustomInput placeholder="Digite seu e-mail" {...register("email",{required:true})} hasError={!!errors?.email} />
+                <CustomInput placeholder="Digite seu e-mail" {...register("email",{required:true , validate: (value) => {
+                        return validator.isEmail(value)
+
+                }})} hasError={!!errors?.email} />
+                {errors?.email?.type === "required" && (
+                    <InputErrorMessage>O e-mail é obrigatório.</InputErrorMessage>
+                )}
+
+
+                {errors?.email?.type === "validate" && (
+                    <InputErrorMessage>Digite um e-mail válido.</InputErrorMessage>
+                ) }
+
              </LoginInputContainer>
              <LoginInputContainer>
                 <p>Senha</p>
                 <CustomInput placeholder="Digite sua senha"  {...register("password",{required:true})} hasError={!!errors?.password}/>
+
+                {errors?.password?.type === "required" && (
+                    <InputErrorMessage>A senha é obrigatória.</InputErrorMessage>
+                )}
              </LoginInputContainer>
 
 
