75e70b3965f8f2388a46447939446cb4852ed298
ChristianSouza12 <christianlmsouza@outlook.com>
2024-09-06 08:53:57 -0300

    chore: add firebase config

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0i46FB3LzGqgvG4aBLaupjPLnWU3reIo",
+  authDomain: "e-commerce-e656a.firebaseapp.com",
+  projectId: "e-commerce-e656a",
+  storageBucket: "e-commerce-e656a.appspot.com",
+  messagingSenderId: "267136656213",
+  appId: "1:267136656213:web:06e3364a5af820c5cac5dc"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
